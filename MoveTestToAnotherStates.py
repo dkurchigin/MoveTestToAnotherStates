a7774c41d0acfc1fd0c10338ffa6faeedaa543b9
dkurchigin from work <kurchigin.dmitry@yandex.ru>
2018-09-07 18:37:06 +0300

    try while...

--- MoveTestToAnotherStates.py
+++ MoveTestToAnotherStates.py
@@ -4,6 +4,8 @@ import os
 directory = '.'
 files = os.listdir(directory)
 
+rule_list = []
+
 finite_rule = "global_interrupts_wrong_phone"
 finite_state = "18"
 
@@ -31,11 +33,55 @@ def print_files_on_dir():
 def find_finite_state(file_path):
     f = open(file_path, 'r', encoding='utf-8')
     for line in f:
-        print (line)
+        pattern_for_state = re.findall(r'^\w+',line)
+        
+        if len(rule_list) == 0:
+            rule_list.append(pattern_for_state)
+        else:
+            #print (len(rule_list))
+            if len(rule_list) == 1:
+                if rule_list[0] != pattern_for_state:
+                    rule_list.append(pattern_for_state)
+                else:
+                    n = 0
+                    while n < 1:
+                        if rule_list[n] != pattern_for_state:
+                            rule_list.append(pattern_for_state)
+                        n += 1
+        #print (pattern_for_state)
+        
 
 
 print_files_on_dir() 
 file_number = int(input())
 find_finite_state(str(files[file_number]))
+print ('')
+print ('')
+print ('===================')
+print (rule_list[0])
+print (rule_list[1])
+#print (rule_list[2])
+print (len(rule_list))
 
-
+first = ["one", "two", "three"]
+second = []
+ 
+for element in first:
+    print (element)
+    if len(second) == 0:
+        second.append(element)
+    else:
+        n = 0
+        while n < len(second):
+            print ("there")
+            if second[n] != element:
+                second.append(element)
+            n += 1    
+    
+        
+print (second)
+#n = 0
+#while n < len(second):
+    #if second[n] != "boo":
+        #second.append("boo")
+    #n += 1
